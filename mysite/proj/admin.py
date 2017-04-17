2ebd90ec43b4360480172f5350675b842208d5fe
Natalie Jane Edson <Natalie Jane Edson>
2017-04-16 18:21:59 -0700

    Gets the models and the admin in working order

--- mysite/proj/admin.py
+++ mysite/proj/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import * 
 
 # Register your models here.
+
+admin.site.register(Blurb)
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Post)
+
+
+
+''' class BookAdmin(admin.ModelAdmin):
+
+class AuthorAdmin(admin.ModelAdmin): '''
--- mysite/proj/models.py
+++ mysite/proj/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model): 
+	pub_date=models.DateTimeField()
+	general_notes=models.TextField(null=True, blank=True)
+	
+
+	def __str__(self): 
+		return (self.general_notes)
+
+class Blurb(models.Model): 
+	pub_date = models.DateTimeField()
+	text = models.TextField(null=True)
+	book = models.ForeignKey('Book')
+	post = models.ForeignKey('Post')
+	reading = models.BooleanField(default=False)
+
+	def __str__(self): 
+		return (self.text)
+
+class Book(models.Model):
+	title = models.CharField(max_length=300)
+	author = models.ForeignKey('Author')
+	general_notes=models.TextField(null=True, blank=True)
+
+	def __str__(self): 
+		return self.title
+
+class Author(models.Model): 
+	first_name = models.CharField(max_length=100, null=True)
+	last_name = models.CharField(max_length=100, null=True)
+	general_notes=models.TextField(null=True, blank=True)
+
+	def __str__(self): 
+		return (self.first_name +" "+ self.last_name)
