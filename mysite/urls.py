4ad3029d1230ba2b8c6c14e542055bc05fdcc5f3
Natalie Jane Edson <Natalie Jane Edson>
2017-04-17 16:49:54 -0700

    changes urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^proj/', include('proj.urls')),
+    url(r'^$', include('proj.urls')),
     url(r'^admin/$', admin.site.urls),
 ]
